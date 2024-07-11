e70f0ccac0c9eec698680857e6826ed87568b6dc
bugzhang <nuptunee@gmail.com>
2024-07-12 03:35:37 +0800

    arco-cli: add package @arco-pro-components/ProTable

---
+++ packages/ProTable/src/demo/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @title 名称
+ * @memberOf 区块
+ * @description 描述你的区块。
+ * @author 物料作者
+ * @package @arco-pro-components/ProTable
+ */
+
+/**
+ * @title 示例
+ */
+export { default as Basic } from './basic';
---
+++ packages/ProTable/stories/stories.jsx
@@ -0,0 +1,5 @@
+export * from '../src/demo';
+
+export default {
+  title: '@arco-pro-components/ProTable',
+};
